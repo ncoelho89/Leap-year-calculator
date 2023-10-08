7fc90a06dfc84c429b7d4bfd9b85ae760a25b66d
ncoelho89 <144150634+ncoelho89@users.noreply.github.com>
2023-10-08 17:20:12 +0000

    Added "!" to make it more exciting

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ year = int(input("Which year would you like to check? "))
 if year % 4 == 0:
   if year % 100 == 0:
     if year % 400 == 0:
-      print("Leap year")
+      print("Leap year!")
     else:
-      print("Not a leap year")
+      print("Not a leap year!")
   else:
-    print("Leap year")
+    print("Leap year!")
 else:
-  print("Not a leap year")
+  print("Not a leap year!")
     
   
 
