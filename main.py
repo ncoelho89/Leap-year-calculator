3f7f520a1fb3770ba2478ff2bac8b712ea2a9524
nicholascoelho8 <26955725-nicholascoelho8@users.noreply.replit.com>
2023-10-08 17:13:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+year = int(input("Which year would you like to check? "))
+
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print("Leap year")
+    else:
+      print("Not a leap year")
+  else:
+    print("Leap year")
+else:
+  print("Not a leap year")
+    
+  
+
+
+
