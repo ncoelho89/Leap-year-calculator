8cdd7dc19fa8ddcce2b6b4995469dc706543740a
ncoelho89 <144150634+ncoelho89@users.noreply.github.com>
2023-10-08 13:38:32 -0400

    Update main.py
    removed "!" from not a leap year since that's not as exciting as a leap year

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ if year % 4 == 0:
     if year % 400 == 0:
       print("Leap year!")
     else:
-      print("Not a leap year!")
+      print("Not a leap year")
   else:
     print("Leap year!")
 else:
-  print("Not a leap year!")
+  print("Not a leap year")
     
   
 
